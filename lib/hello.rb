1cb48a1d0fb54d0d1b876ad083a34142120075ac
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-26 18:50:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,7 @@
 def hello_t(array)
   if block_given?
       i = 0
+<<<<<<< HEAD
 
       while i < array.length
           yield(array[i])
@@ -13,6 +14,20 @@ def hello_t(array)
       end
     end
 
+=======
+
+      while i < array.length
+          yield(array[i])
+          i = i + 1
+        end
+     
+        array
+      else
+        puts "Hey! No block was given!"
+      end
+    end
+    
+>>>>>>> d3bf784b18f7ebdcd6848a86788cf927991b146c
 # hello_t(["Tim", "Tom", "Jim"]) do |name|
 #   if name.start_with?("T")
 #     puts "Hi, #{name}"
